d4ddb96228b6b87a668038e7a93fde0ada71bda3
Bartłomiej Sikora <bartlomiej.sikora@student.wat.edu.pl>
2018-12-02 19:44:53 +0100

    -add method for logging

--- src/main/java/com/example/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/demo/mycoolapp/rest/FunRestController.java
@@ -1,14 +1,33 @@
 package com.example.demo.mycoolapp.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import com.example.demo.mycoolapp.DbObjects.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FunRestController {
+
+    private static final String template = "Hello, %s!";
+    private long  id = 1;
+    private ObjectMapper mapper = new ObjectMapper();
+
     @GetMapping("/")
     public String sayHello(){
-        return "HelloWorld! " + LocalDateTime.now();
+        return "{}";
     }
+    @GetMapping("/user")
+    public String loggingMethod(@RequestParam(value="user", defaultValue="") String user , @RequestParam(value="password", defaultValue="") String password) {
+        User u = new User(id  ,user , password);
+        try {
+            return  mapper.writeValueAsString(u);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+
 }
